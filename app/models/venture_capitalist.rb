67c9dbaed46ad5c887381d1b6ba6260581573ff8
RubenV-Dev <r.will16@hotmail.com>
2019-10-14 18:06:22 -0400

    1

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -40,6 +40,7 @@ class VentureCapitalist
             if memo == nil
                 self.funding_rounds[0]
             end
+            binding.pry
             if (funding_round.investment > memo.investment)
                 memo = funding_round
                 memo
