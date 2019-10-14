f43588a1bc7cce8e7274a41b5c7391e5976dcf2c
RubenV-Dev <r.will16@hotmail.com>
2019-10-14 10:35:04 -0400

    1

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,61 @@
 class VentureCapitalist
+    attr_accessor :name, :total_worth
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def tres_commas_club
+        self.all.select do |one_venture|
+            one_venture.total_worth > 1,000,000
+        end
+    end
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select do |funding_round|
+            funding_round.venture_capitalist == self
+        end
+    end
+
+    def portfolio
+        startup_array = self.funding_rounds.map do |funding_round|
+            funding_round.startup
+        end
+        startup_array.uniq
+    end
+
+    def biggest_investment
+        self.funding_rounds.reduce(nil) do |memo, funding_round|
+            if memo == nil
+                self.funding_round[0]
+            end
+            if funding_round.investment.to_f > memo.investment.to_f
+                memo = funding_round
+            end
+            memo
+        end
+    end
+
+    def invested(domain)
+        funding_round_by_domain = self.funding_round.select do |funding_round|
+            funding_round.startup.domain == domain
+        end
+
+        funding_round_by_domain.reduce(0) do |memo, funding_round|
+            memo = memo += funding_round.investment
+            memo
+        end
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,10 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+companyA = Startup.new("Tom","companyFounderA", "domainA")
+companyB = Startup.new("Forest", "companyFounderB", "domainB")
+companyC = Startup.new("Herald", "companyFounderC", "domainC")
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
