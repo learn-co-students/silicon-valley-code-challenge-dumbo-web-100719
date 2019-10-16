af177bd45e9cc4cbf1ad866ac0aebc547d41bdef
Vannida Lim <vannida.lim@gmail.com>
2019-10-16 17:28:31 -0400

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,72 @@
 class Startup
+    attr_reader :founder, :domain 
+    attr_accessor :name
+
+    @@all = []
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self 
+    end
+
+    def pivot(domain, name)
+        @domain = domain 
+        @name = name
+    end
+
+    def self.all
+        @@all 
+    end
+
+    def self.find_by_founder(founder)
+        self.all.find do |startup|
+            startup.founder == founder
+        end
+    end
+
+    def self.domains
+        self.all.map do |startup|
+            startup.domain
+        end
+    end
+
+    def sign_contract(type, investment, venture_capitalist)
+        FundingRound.new(self,venture_capitalist, type, investment)
+    end
+
+    def funding_for_curr_startup 
+        FundingRound.all.select do |funding_round|
+            funding_round.startup == self
+        end
+    end
+
+    def num_funding_rounds
+        self.funding_for_curr_startup.count 
+    end
+
+    def total_funds
+        self.funding_for_curr_startup.reduce(0) do |acc, curr_funding_round|
+            acc += curr_funding_round.investment
+        end
+    end
+
+    def investors
+        investors_list = self.all.select do |funding_round|
+            funding_round.venture_capitalist
+        end
+        investors_list.uniq!
+    end
+
+    def big_investors
+        get_vc = funding_for_curr_startup.map do |funding_round|
+            funding_round.venture_capitalist
+        end
+       tres_commas_vc = get_vc.select do |vc|
+            binding.pry
+            vc.tres_commas_club
+       end
+       tres_commas_vc.uniq!
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,14 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+airbnb = Startup.new("Airbnb", "Joe", "services")
+we_work = Startup.new("WeWork", "Minseo", "wework.com")
 
+kim_minseo = VentureCapitalist.new("Minseo Kim", 50)
+lim_vannida = VentureCapitalist.new("Vannida Lim", 10_000_000_000)
+
+seed_1 = FundingRound.new(airbnb, kim_minseo, "seed a", 2_000_000)
+seed_2 = FundingRound.new(we_work, lim_vannida, "angel", 1_000)
+seed_3 = FundingRound.new(airbnb, kim_minseo, "seed a", 4_000_000)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
